ecfd5ae1bcf100c3393a524ad6f31cbd870da68f
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-11-27 21:32:01 +0700

    decode token

---
+++ controllers/verifyToken.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers["Authorization"];
+    if(!token) {
+        return res.status(401).send({ auth: false, message: "No token provided" });
+    }
+    // const decode = await jwt.verify(token, config.secret);
+    // req.userId = decode.id;
+
+    // next();
+}
+
+module.exports = verifyToken;
