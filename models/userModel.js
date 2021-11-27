08d208a4ff7234222ed0253e4253fc85bece32c9
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-11-27 20:56:35 +0700

    user model

---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new Schema(
+    {
+        username: String,
+        email: String,
+        password: String
+    }
+);
+
+UserSchema.methods.encryptPassworf = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+};
+
+UserSchema.methods.validatePassword = () => {
+    return bcrypt.compare(password, this.password);
+
+}
+
+module.exports = model("User", UserSchema);
