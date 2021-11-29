d1c82d735cb0cd71ecc8b7c4942499a0dbabefa6
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-11-29 20:07:58 +0700

    signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,4 +7,54 @@ const verifyToken = require("../controllers/verifyToken");
 const jwt = require("jsonwebtoken");
 const config = require("../config");
 
-// router.post("")
+router.post("/signup", (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const user = new User({
+            username,
+            email,
+            password
+        });
+
+        user.password = await user.encryptPassword(password);
+        await user.save();
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+            },
+            config.secret,
+            {
+                expiresIn: "1h"
+            })
+        res.status(200).json({ auth: true, token: token })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("There was a problem signup");
+    }
+});
+
+// router.post("/signin", (req, res) => {
+//     try {
+//         const user = await User.findOne({email: req.body.email})
+//         if (!user) {
+//             return res.status(404).send("The email doesn't exist");
+//         }
+
+//         const validPassword = await (req.body.password, user.password);
+//         if (!validPassword) {
+//             return res.status(401).send({auth: false, token:null})
+//         }
+//         const token = jwt.sign({ id: user._id }, config.secret, { expiresIn: "1h"})
+//         res.status(200).json({ auth: true, token: token });
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).send("There is a problem")
+//     }
+// })
+
+// router.get("/logout", (req, res) => {
+//     res.status(200).json({ auth: false, token: null })
+// })
